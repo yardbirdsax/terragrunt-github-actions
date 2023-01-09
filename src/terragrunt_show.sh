1fce012ec00e68180039f35fe744a4257ac1ef76
Sean Smith <sean_smith@discovery.com>
2023-01-09 10:29:08 -0800

    minor tweaks

--- src/terragrunt_show.sh
+++ src/terragrunt_show.sh
@@ -5,15 +5,12 @@ function terragruntShow {
   echo "show: info: planning Terragrunt configuration in ${tfWorkingDir}"
   showOutput=$(${tfBinary} show ${*})
   showExitCode=${?}
-  planHasChanges=false
-  planCommentStatus="Failed"
 
   # Exit code of 0 indicates success with no changes. Print the output and exit.
   if [ ${showExitCode} -eq 0 ]; then
     echo "plan: info: successfully showing Terragrunt configuration in ${tfWorkingDir}"
     echo "${showOutput}"
     echo
-    echo ::set-output name=tf_actions_plan_has_changes::${planHasChanges}
 
   else
     echo "show: error: failed to show Terragrunt configuration in ${tfWorkingDir}"

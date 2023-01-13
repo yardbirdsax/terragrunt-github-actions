bf1c69fa1c67dc273d46259e9646278700ceba48
Sean Smith <sean_smith@discovery.com>
2023-01-13 14:35:27 -0800

    debug output added

--- src/terragrunt_json_file.sh
+++ src/terragrunt_json_file.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 function terragruntJsonFile {
-  # Gather the output of `terragrunt plan`.
   echo "show_json: info: outputting plan in json format to ${tfWorkingDir}/tf_plan.json"
+  pwd
+  tree
   ${tfBinary} show -json ${*} > tf_plan.json
   showExitCode=${?}
 

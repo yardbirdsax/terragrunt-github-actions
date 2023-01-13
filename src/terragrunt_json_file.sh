cede5db31d066cf7f6da2ad07389402c0634596f
Sean Smith <sean_smith@discovery.com>
2023-01-13 14:50:04 -0800

    fix: bug with json file output

--- src/terragrunt_json_file.sh
+++ src/terragrunt_json_file.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 function terragruntJsonFile {
-  # Gather the output of `terragrunt plan`.
   echo "show_json: info: outputting plan in json format to ${tfWorkingDir}/tf_plan.json"
-  ${tfBinary} show -json ${*} > ${tfWorkingDir}/tf_plan.json
+  ${tfBinary} show -json ${*} > tf_plan.json
   showExitCode=${?}
 
   exit ${showExitCode}

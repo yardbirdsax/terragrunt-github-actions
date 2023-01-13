e6837f40fe15236a7e72663f1c605929bc7a0529
Sean Smith <sean_smith@discovery.com>
2023-01-13 14:36:09 -0800

    undo changes sent to upstream accidentally

--- src/terragrunt_json_file.sh
+++ src/terragrunt_json_file.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 function terragruntJsonFile {
+  # Gather the output of `terragrunt plan`.
   echo "show_json: info: outputting plan in json format to ${tfWorkingDir}/tf_plan.json"
-  pwd
-  tree
-  ${tfBinary} show -json ${*} > tf_plan.json
+  ${tfBinary} show -json ${*} > ${tfWorkingDir}/tf_plan.json
   showExitCode=${?}
 
   exit ${showExitCode}
